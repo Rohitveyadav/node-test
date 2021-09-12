aa981441ad9dc7ec8e580a2b1a33307a87d20f75
rohitveyadav <rohitveyadav@gmail.com>
2021-09-12 21:45:00 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+console.log("git tes")
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
